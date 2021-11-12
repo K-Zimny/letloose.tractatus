06ac761422221617bed4a08c29ad939af23f7e89
kzim <k.zimny1994@gmail.com>
2021-11-12 14:35:16 -0600

    Updates before branch move to main

--- picostrap5-child-base/functions.php
+++ picostrap5-child-base/functions.php
@@ -94,4 +94,63 @@ wp_enqueue_script('swiper_js');
 add_action("wp_head",function(){ ?> 
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
    <?php }); 
-   
+  
+  
+// ------------------------------
+// -----------Remove woocommerce on non woo commercepages-----------
+// ------------------------------
+
+add_action( 'template_redirect', 'bt_remove_woocommerce_styles_scripts', 999 );
+/**
+ * Remove Woo Styles and Scripts from non-Woo Pages
+ * @link https://gist.github.com/DevinWalker/7621777#gistcomment-1980453
+ * @since 1.7.0
+ */
+function bt_remove_woocommerce_styles_scripts() {
+
+        // Skip Woo Pages
+        if ( is_woocommerce() || is_cart() || is_checkout() || is_account_page() ) {
+                return;
+        }
+        // Otherwise...
+        remove_action('wp_enqueue_scripts', [WC_Frontend_Scripts::class, 'load_scripts']);
+        remove_action('wp_print_scripts', [WC_Frontend_Scripts::class, 'localize_printed_scripts'], 5);
+        remove_action('wp_print_footer_scripts', [WC_Frontend_Scripts::class, 'localize_printed_scripts'], 5);
+}
+
+// ------------------------------
+// -----------Remove jetpack css-----------
+// ------------------------------
+
+// First, make sure Jetpack doesn't concatenate all its CSS
+add_filter( 'jetpack_implode_frontend_css', '__return_false' );
+
+// Then, remove each CSS file, one at a time
+function jeherve_remove_all_jp_css() {
+  wp_deregister_style( 'AtD_style' ); // After the Deadline
+  wp_deregister_style( 'jetpack_likes' ); // Likes
+  wp_deregister_style( 'jetpack_related-posts' ); //Related Posts
+  wp_deregister_style( 'jetpack-carousel' ); // Carousel
+  wp_deregister_style( 'grunion.css' ); // Grunion contact form
+  wp_deregister_style( 'the-neverending-homepage' ); // Infinite Scroll
+  wp_deregister_style( 'infinity-twentyten' ); // Infinite Scroll - Twentyten Theme
+  wp_deregister_style( 'infinity-twentyeleven' ); // Infinite Scroll - Twentyeleven Theme
+  wp_deregister_style( 'infinity-twentytwelve' ); // Infinite Scroll - Twentytwelve Theme
+  wp_deregister_style( 'noticons' ); // Notes
+  wp_deregister_style( 'post-by-email' ); // Post by Email
+  wp_deregister_style( 'publicize' ); // Publicize
+  wp_deregister_style( 'sharedaddy' ); // Sharedaddy
+  wp_deregister_style( 'sharing' ); // Sharedaddy Sharing
+  wp_deregister_style( 'stats_reports_css' ); // Stats
+  wp_deregister_style( 'jetpack-widgets' ); // Widgets
+  wp_deregister_style( 'jetpack-slideshow' ); // Slideshows
+  wp_deregister_style( 'presentations' ); // Presentation shortcode
+  wp_deregister_style( 'jetpack-subscriptions' ); // Subscriptions
+  wp_deregister_style( 'tiled-gallery' ); // Tiled Galleries
+  wp_deregister_style( 'widget-conditions' ); // Widget Visibility
+  wp_deregister_style( 'jetpack_display_posts_widget' ); // Display Posts Widget
+  wp_deregister_style( 'gravatar-profile-widget' ); // Gravatar Widget
+  wp_deregister_style( 'widget-grid-and-list' ); // Top Posts widget
+  wp_deregister_style( 'jetpack-widgets' ); // Widgets
+}
+add_action('wp_print_styles', 'jeherve_remove_all_jp_css' );
